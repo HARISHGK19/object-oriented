b236799215ec677777aeffbe269cba4095bfe57b
HARISHGK19 <113233000+HARISHGK19@users.noreply.github.com>
2023-08-24 08:41:51 +0530

    Add files via upload

---
+++ exp1/APPLICANT.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class APPLICANT
+ */
+public class APPLICANT {
+
+  //
+  // Fields
+  //
+
+  private void applicant_name;
+  private void applicant_details;
+  
+  //
+  // Constructors
+  //
+  public APPLICANT () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of applicant_name
+   * @param newVar the new value of applicant_name
+   */
+  public void setApplicant_name (void newVar) {
+    applicant_name = newVar;
+  }
+
+  /**
+   * Get the value of applicant_name
+   * @return the value of applicant_name
+   */
+  public void getApplicant_name () {
+    return applicant_name;
+  }
+
+  /**
+   * Set the value of applicant_details
+   * @param newVar the new value of applicant_details
+   */
+  public void setApplicant_details (void newVar) {
+    applicant_details = newVar;
+  }
+
+  /**
+   * Get the value of applicant_details
+   * @return the value of applicant_details
+   */
+  public void getApplicant_details () {
+    return applicant_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void apply_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void submission_of_proof()
+  {
+  }
+
+
+}
---
+++ exp1/ENQUIRY_OFFICE.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Class ENQUIRY_OFFICE
+ */
+public class ENQUIRY_OFFICE {
+
+  //
+  // Fields
+  //
+
+  private void verification_of_details;
+  
+  //
+  // Constructors
+  //
+  public ENQUIRY_OFFICE () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verification_of_details
+   * @param newVar the new value of verification_of_details
+   */
+  public void setVerification_of_details (void newVar) {
+    verification_of_details = newVar;
+  }
+
+  /**
+   * Get the value of verification_of_details
+   * @return the value of verification_of_details
+   */
+  public void getVerification_of_details () {
+    return verification_of_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void issue_of_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void renewal_os_passport()
+  {
+  }
+
+
+}
